992c3645e867b53fbf7c75773a8794ae86140f0f
izhdanko <izhdanko@rambler.ru>
2023-09-05 05:00:41 +0300

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+
+  before_action :find_post_id, only: [:create, :destroy]
+
+  def create
+    @comment = @post.comments.create(params[:comment].permit(:name, :comment))
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
+
+  private
+
+  def find_post_id
+    @post = Post.find(params[:post_id])
+  end
+
+end
